4061c9ae9712efef0f8687a57aa0a04a7cee072e
Solalunara <greenberg04@gmail.com>
2025-02-23 07:10:29 +0000

    recover door controller

--- Assets/Scripts/CodeDisplayOutput.cs
+++ Assets/Scripts/CodeDisplayOutput.cs
@@ -14,6 +14,8 @@ public class CodeDisplayOutput : MonoBehaviour
 
     public void UpdateText( string s )
     {
+        if ( !m_pText )
+            m_pText = GetComponent<TMP_Text>();
         m_pText.text = s;
     }
     
